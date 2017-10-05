a19aa652d99400850a1224f157ba5556cc385e20
david async <davidasync@gmail.com>
2017-10-05 22:54:12 +0700

    First commit

---
+++ constant.js
@@ -0,0 +1,47 @@
+exports.wordpressLinks = [
+  {
+    title: 'Cara Migrasi Hosting WordPress Menggunakan Duplicator',
+    url: 'https://nginy.net/tutorial/wordpress/migrasi-hosting-wordpress-duplicator/',
+    date: '24 Sep 2017'
+  },
+  {
+    title: 'Cara Install WordPress Offline Menggunakan XAMPP',
+    url: 'https://nginy.net/tutorial/wordpress/install-wordpress-offline/',
+    date: '23 Sep 2017'
+  },
+  {
+    title: 'Cara Render Page WordPress Dalam Static File',
+    url: 'https://nginy.net/tutorial/wordpress/render-page-wordpress-dalam-static-file/',
+    date: '16 Sep 2017'
+  },
+  {
+    title: 'Cara Embed Tweet Ke Dalam Post WordPress',
+    url: 'https://nginy.net/tutorial/wordpress/cara-embed-tweet-ke-dalam-post-wordpress/',
+    date: '02 Jul 2017'
+  },
+  {
+    title: 'Cara Embed Video Youtube Ke Dalam Post WordPress',
+    url: 'https://nginy.net/tutorial/wordpress/cara-embed-video-youtube-ke-dalam-post-wordpress/',
+    date: '01 Jul 2017'
+  },
+  {
+    title: 'Cara Embed Instagram Pada Post WordPress',
+    url: 'https://nginy.net/tutorial/wordpress/cara-embed-instagram-pada-wordpress/',
+    date: '27 Jun 2017'
+  },
+  {
+    title: '7 Cara Mempercepat Loading Page Website Pada WordPress',
+    url: 'https://nginy.net/tutorial/wordpress/7-cara-mempercepat-loading-page-website-pada-wordrpress/',
+    date: '02 Apr 2017'
+  },
+  {
+    title: '7 Plugins WordPress yang Harus Diinstall Ketika Pertama kali Membuat WordPress',
+    url: 'https://nginy.net/tutorial/wordpress/7-plugins-wordpress-yang-harus-diinstall-ketika-pertama-kali-membuat-wordpress/',
+    date: '25 mar 2017'
+  },
+  {
+    title: '7 Langkah Tutorial Membuat WordPress Self Hosting Gratis Dalam Waktu Kurang Dari 30 Menit',
+    url: 'https://nginy.net/tutorial/wordpress/tutorial-membuat-wordpress-self-hosting-gratis/',
+    date: '26 Feb 2017'
+  }
+];

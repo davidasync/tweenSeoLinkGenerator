477b82e9854e95ea022da7ca7849b0b5e635dd2d
david async <davidasync@gmail.com>
2017-10-11 08:31:00 +0700

    At trivia link for english version

---
+++ constants/en/triviaLinks.js
@@ -0,0 +1,42 @@
+module.exports.triviaLinks = [
+  {
+    title: '7 Instagram’s Facts That Might You don’t Know',
+    url: 'https://nginy.net/trivia-en/instagrams-facts/',
+    date: '22 Jul 2017'
+  },
+  {
+    title: 'The Top 7 of The Most Visited Website in the World',
+    url: 'https://nginy.net/trivia-en/visited-websites/',
+    date: '24 Feb 2017'
+  },
+  {
+    title: '7 Mark Zuckerberg’s Facts That Can Make You Surprised',
+    url: 'https://nginy.net/trivia-en/mark-zuckerbergs-fact/',
+    date: '21 Feb 2017'
+  },
+  {
+    title: '7 Steve Jobs’s Facts That Maybe You don’t Know',
+    url: 'https://nginy.net/trivia-en/7-steve-jobs-facts/',
+    date: '20 Feb 2017'
+  },
+  {
+    title: '7 Youtube’s Secret Tricks That Maybe You Guys Don’t Know',
+    url: 'https://nginy.net/trivia-en/7-youtube-secret-tricks/',
+    date: '19 Feb 2017'
+  },
+  {
+    title: 'Google’s Secret Tricks That Make You Guys Want To Prove It',
+    url: 'https://nginy.net/trivia-en/7-google-secret-tricks/',
+    date: '18 Feb 2017'
+  },
+  {
+    title: '7 Google’s secret tricks that helps our activity',
+    url: 'https://nginy.net/trivia-en/7-google-tricks-secret/',
+    date: '16 Feb 2017'
+  },
+  {
+    title: '7 Google’s Facts That Maybe You Guys Don’t Know',
+    url: 'https://nginy.net/trivia-en/7-google-facts/',
+    date: '16 Feb 2017'
+  },
+]
